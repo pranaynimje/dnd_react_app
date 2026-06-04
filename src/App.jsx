3e3d894a0fc8d5446ab0fc975499c48b12cd5844
pranaynimje <pranay.nimje@logward.com>
2026-06-04 15:53:57 +0530

    Revert App.jsx to original dnd_dashboard (persona hub moved to separate repo)

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import DndDashboard from './dnd_dashboard'
-import DnDPersonaHub from './dnd_persona_hub'
-
-// Toggle: set to true to preview the new persona-based hub
-const SHOW_PERSONA_HUB = true
 
 function App() {
-  return SHOW_PERSONA_HUB ? <DnDPersonaHub /> : <DndDashboard />
+  return <DndDashboard />
 }
 
 export default App
